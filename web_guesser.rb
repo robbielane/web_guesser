6af032818036767f5e9ecf03954833494bd00f7f
Robbie Lane <lane.robbie@gmail.com>
2015-10-05 22:32:19 -0600

    added colors!

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,25 +5,29 @@ SECRET_NUMBER = rand(100)
 
 def guess_check(guess)
   if guess == 0
-    "Make a guess (0 - 100)"
+    "Guess a number (0 - 100)"
   else
     if guess > (SECRET_NUMBER + 5)
-      "Way too high!"
+      ["Way too high!", "#BF3F46"]
     elsif guess < (SECRET_NUMBER - 5)
-      "Way too low!"
+      ["Way too low!", "#BF3F46"]
     elsif guess > SECRET_NUMBER
-      "Too high!"
+      ["Too high!", "#FF5A64"]
     elsif guess < SECRET_NUMBER
-      "Too low!"
+      ["Too low!", "#FF5A64"]
     else
-      "You guessed it!"
+      ["You guessed it!", "#4ABF5A"]
     end
   end
 end
 
 get '/' do
   guess = params['guess'].to_i
-  message = guess_check(guess)
-  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message}
+  message, color = guess_check(guess)
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message, :color => color}
 
 end
+
+#BF3F46 Dark red
+#FF7A8A Light red
+#4ABF5A Green
